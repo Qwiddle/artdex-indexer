d5e7d205ae4f923e633badc9131b4aa1a192efaa
Andrew Rubenstein <andjruben@gmail.com>
2023-01-02 23:01:24 -0500

    create DB models for pools & swaps

--- dipdup-indexer/src/artdex_indexer/models.py
+++ dipdup-indexer/src/artdex_indexer/models.py
@@ -2,6 +2,20 @@ from tortoise import fields
 from dipdup.models import Model
 
 
-class User(Model):
-    address = fields.CharField(36, pk=True)
-    balance = fields.IntField()
+class Pool(Model):
+    contract = fields.CharField(36, pk=True)
+    id_list = fields.JSONField()
+    fee = fields.BigIntField()
+    delta = fields.BigIntField()
+    payment_token_fa2 = fields.CharField(36)
+    payment_token_id = fields.IntField()
+    reserve = fields.BigIntField()
+    spot_price = fields.BigIntField()
+    collection_address = fields.CharField(36)
+  
+class Swap(Model):
+    contract = fields.CharField(36)
+    nft_ids = fields.CharField(255)
+    max_tokens_in = fields.BigIntField(null=True)
+    min_tokens_out = fields.BigIntField(null=True)
+    to = fields.CharField(36)
