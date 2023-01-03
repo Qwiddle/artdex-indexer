103f3355085b26182bc6297375d6695ab03dd06f
Andrew Rubenstein <andjruben@gmail.com>
2023-01-03 00:19:04 -0500

    handle withdrawal & deposit of tokens

--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_deposit_tokens.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_deposit_tokens.py
@@ -1,3 +1,5 @@
+import artdex_indexer.models as models
+
 from artdex_indexer.types.single_artdex_pool.parameter.deposit_tokens import DepositTokensParameter
 from artdex_indexer.types.single_artdex_pool.storage import SingleArtdexPoolStorage
 from dipdup.context import HandlerContext
@@ -8,4 +10,16 @@ async def on_artdex_pool_deposit_tokens(
     ctx: HandlerContext,
     deposit_tokens: Transaction[DepositTokensParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = deposit_tokens.data.target_address
+    amount = deposit_tokens.parameter.__root__
+
+    try:
+        pool = await models.Pool.get(contract=contract)
+        
+        pool.reserve = pool.reserve + int(amount)
+        
+        await pool.save()
+    
+    except Exception as e:
+        print("Error in on_artdex_pool_deposit_nfts " + contract)
+        print(e)
--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_withdraw_tokens.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_withdraw_tokens.py
@@ -1,3 +1,5 @@
+import artdex_indexer.models as models
+
 from artdex_indexer.types.single_artdex_pool.parameter.withdraw_tokens import WithdrawTokensParameter
 from artdex_indexer.types.single_artdex_pool.storage import SingleArtdexPoolStorage
 from dipdup.context import HandlerContext
@@ -8,4 +10,15 @@ async def on_artdex_pool_withdraw_tokens(
     ctx: HandlerContext,
     withdraw_tokens: Transaction[WithdrawTokensParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = withdraw_tokens.data.target_address
+    amount = withdraw_tokens.parameter.__root__
+
+    try:
+        pool = await models.Pool.get(contract=contract)
+
+        pool.reserve = pool.reserve - int(amount)
+        await pool.save()
+    
+    except Exception as e:
+        print("Error in on_artdex_pool_withdraw_tokens " + contract)
+        print(e)
