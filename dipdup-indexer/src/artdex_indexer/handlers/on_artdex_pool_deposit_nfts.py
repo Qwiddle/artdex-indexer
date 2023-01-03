29d3f8e22613e769a5fdc1786330aa577d59ce4c
Andrew Rubenstein <andjruben@gmail.com>
2023-01-02 23:30:40 -0500

    handle withdrawal & deposit of nfts

--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_deposit_nfts.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_deposit_nfts.py
@@ -1,3 +1,5 @@
+import artdex_indexer.models as models
+
 from artdex_indexer.types.single_artdex_pool.parameter.deposit_nf_ts import DepositNFTsParameter
 from artdex_indexer.types.single_artdex_pool.storage import SingleArtdexPoolStorage
 from dipdup.context import HandlerContext
@@ -8,4 +10,23 @@ async def on_artdex_pool_deposit_nfts(
     ctx: HandlerContext,
     deposit_nfts: Transaction[DepositNFTsParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = deposit_nfts.data.target_address
+    nft_id_list = deposit_nfts.parameter.__root__
+
+    try:
+        pool = await models.Pool.get(contract=contract)
+
+        # check if id exists in pool already, 
+        # if not, set dict value of key id to 1. or, increase value by 1.
+        
+        for id in nft_id_list:
+            if pool.id_list.get(id) == None:
+                pool.id_list[id] = 1
+            else:
+                pool.id_list[id] = pool.id_list[id] + 1
+
+        await pool.save()
+
+    except Exception as e:
+        print("Error in on_artdex_pool_deposit_nfts " + contract)
+        print(e)
--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_withdraw_nfts.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_withdraw_nfts.py
@@ -1,3 +1,5 @@
+import artdex_indexer.models as models
+
 from artdex_indexer.types.single_artdex_pool.parameter.withdraw_nf_ts import WithdrawNFTsParameter
 from artdex_indexer.types.single_artdex_pool.storage import SingleArtdexPoolStorage
 from dipdup.context import HandlerContext
@@ -8,4 +10,25 @@ async def on_artdex_pool_withdraw_nfts(
     ctx: HandlerContext,
     withdraw_nfts: Transaction[WithdrawNFTsParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = withdraw_nfts.data.target_address
+    nft_id_list = withdraw_nfts.parameter.__root__
+
+    try:
+        pool = await models.Pool.get(contract=contract)
+        
+        # check if value of key id is = 1.
+        # if so, delete item from the id_list dict. or, decrease the value by one.
+
+        for id in nft_id_list:
+            value = pool.id_list[id]
+
+            if int(value) == 1:
+                del pool.id_list[id]
+            else:
+                pool.id_list[id] = int(value) - 1
+
+        await pool.save()
+        
+    except Exception as e:
+        print("Error in on_artdex_pool_withdraw_nfts " + contract)
+        print(e)
