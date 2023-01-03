3942f66b0cdbe990281a2ddfb6c848a0e97d4196
Andrew Rubenstein <andjruben@gmail.com>
2023-01-03 00:29:12 -0500

    handle nft2token and token2nft calls

--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_nft_to_token.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_nft_to_token.py
@@ -10,4 +10,37 @@ async def on_artdex_pool_nft_to_token(
     ctx: HandlerContext,
     nft_to_token: Transaction[NFT2TokenParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = nft_to_token.data.target_address
+    spot_price = nft_to_token.data.storage['spot_price']
+    reserve = nft_to_token.data.storage['reserve']
+    nft_id_list = nft_to_token.parameter.nft_ids
+    min_tokens_out = nft_to_token.parameter.min_tokens_out
+    _to=nft_to_token.parameter._to
+
+    try:
+        await models.Swap(
+          contract=contract,
+          nft_ids=nft_id_list,
+          min_tokens_out=min_tokens_out,
+          to=_to
+        ).save()
+
+        pool = await models.Pool.get(contract=contract)
+
+        # check if id exists in pool already, 
+        # if not, set dict value of key id to 1. or, increase value by 1.
+
+        for id in nft_id_list:
+            if pool.id_list.get(id) == None:
+                pool.id_list[id] = 1
+            else:
+                pool.id_list[id] = pool.id_list[id] + 1
+        
+        pool.reserve = reserve
+        pool.spot_price = spot_price
+
+        await pool.save()
+
+    except Exception as e:
+        print("Error in on_artdex_pool_nft_to_token " + contract)
+        print(e)
--- dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_token_to_nft.py
+++ dipdup-indexer/src/artdex_indexer/handlers/on_artdex_pool_token_to_nft.py
@@ -1,3 +1,5 @@
+import artdex_indexer.models as models
+
 from artdex_indexer.types.single_artdex_pool.parameter.token_to_nft import Token2NFTParameter
 from artdex_indexer.types.single_artdex_pool.storage import SingleArtdexPoolStorage
 from dipdup.context import HandlerContext
@@ -8,4 +10,39 @@ async def on_artdex_pool_token_to_nft(
     ctx: HandlerContext,
     token_to_nft: Transaction[Token2NFTParameter, SingleArtdexPoolStorage],
 ) -> None:
-    ...
+    contract = token_to_nft.data.target_address
+    spot_price = token_to_nft.data.storage['spot_price']
+    reserve = token_to_nft.data.storage['reserve']
+    nft_id_list = token_to_nft.parameter.nft_ids
+    max_tokens_in = token_to_nft.parameter.max_tokens_in
+    _to=token_to_nft.parameter._to
+
+    try:
+        await models.Swap(
+          contract=contract,
+          nft_ids=nft_id_list,
+          max_tokens_in=max_tokens_in,
+          to=_to
+        ).save()
+
+        pool = await models.Pool.get(contract=contract)
+
+        # check if value of key id is = 1.
+        # if so, delete item from the id_list dict. or, decrease the value by one.
+        
+        for id in nft_id_list:
+            value = pool.id_list[id]
+
+            if value == 1:
+                del pool.id_list[id]
+            else:
+                pool.id_list[id] = value - 1
+
+        pool.reserve = reserve
+        pool.spot_price = spot_price
+
+        await pool.save()
+
+    except Exception as e:
+        print("Error in on_artdex_pool_token_to_nft " + contract)
+        print(e)
