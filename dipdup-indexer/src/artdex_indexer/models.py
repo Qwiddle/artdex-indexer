6827226c4cfbc13befad5c374146a41b01bc5182
Andrew Rubenstein <andjruben@gmail.com>
2023-01-04 00:58:27 -0500

    define indexes for our models

--- dipdup-indexer/src/artdex_indexer/models.py
+++ dipdup-indexer/src/artdex_indexer/models.py
@@ -11,11 +11,11 @@ class Pool(Model):
     payment_token_id = fields.IntField()
     reserve = fields.BigIntField()
     spot_price = fields.BigIntField()
-    collection_address = fields.CharField(36)
+    collection_address = fields.CharField(36, index=True)
   
 class Swap(Model):
-    contract = fields.CharField(36)
+    contract = fields.CharField(36, index=True)
     nft_ids = fields.CharField(255)
     max_tokens_in = fields.BigIntField(null=True)
     min_tokens_out = fields.BigIntField(null=True)
-    to = fields.CharField(36)
+    to = fields.CharField(36, index=True)
